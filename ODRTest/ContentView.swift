f9dffc3639bbd8e5ea94b01d27a4596f05f29e12
Ilias Mirzoiev <iliasmirzoiev9@gmail.com>
2024-01-04 10:04:35 +0200

    Initial Commit

---
+++ ODRTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ODRTest
+//
+//  Created by Ilias Mirzoiev on 04.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ODRTest/ODRTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ODRTestApp.swift
+//  ODRTest
+//
+//  Created by Ilias Mirzoiev on 04.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ODRTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
